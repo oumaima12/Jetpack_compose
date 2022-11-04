8ef7a1d5785fc9fb4042207280637b5eb52ad38b
oumaima_makhlouk <oumaima.makhlouk@ibm.com>
2022-11-04 17:17:39 +0100

    view-model-demo: Starter project

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.jetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.jetpackcompose.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.countText.text = count.toString()
+        binding.button.setOnClickListener {
+            count++
+            binding.countText.text = count.toString()
+        }
     }
 }
