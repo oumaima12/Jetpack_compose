c4da90062e2abb88a2ba7e62af2c0916e6d0de96
oumaima_makhlouk <oumaima.makhlouk@ibm.com>
2022-11-15 16:17:58 +0100

    live-data-demo : Adding live data

--- app/src/main/java/com/example/jetpackcompose/MainActivityViewModel.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivityViewModel.kt
@@ -1,20 +1,20 @@
 package com.example.jetpackcompose
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
 
-    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData: LiveData<Int>
+        get() = total
 
     init {
-        total = startingTotal
-    }
-
-    fun getTotal(): Int {
-        return total
+        total.value = startingTotal
     }
 
     fun setTotal(input: Int) {
-        total += input
+        total.value = (total.value)?.plus(input)
     }
 }
