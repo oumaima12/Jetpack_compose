27af4307b12f9a12e06413072d798b613c79bb93
oumaima_makhlouk <oumaima.makhlouk@ibm.com>
2022-11-30 18:18:09 +0100

    two-way-data-binding: two way data binding demo

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.jetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.jetpackcompose.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        binding.myViewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
