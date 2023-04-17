d98e58140a12e09e2a56e10f7a178e17a2ca9046
oumaima_makhlouk <oumaima.makhlouk@ibm.com>
2023-04-17 17:41:38 +0000

    coroutines-demo : Display on UI using withContext

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -2,40 +2,37 @@ package com.example.jetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
+import com.example.jetpackcompose.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
-    private lateinit var btnDownloadUserData : Button
-    private lateinit var btnCount : Button
-    private lateinit var tvCount : TextView
+    private lateinit var binding : ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnDownloadUserData = findViewById(R.id.btnDownloadUserData)
-        btnCount = findViewById(R.id.btnCount)
-        tvCount = findViewById(R.id.tvCount)
-
-        btnCount.setOnClickListener {
-            tvCount.text = count++.toString()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main )
+        binding.btnCount.setOnClickListener {
+            binding.tvCount.text = count++.toString()
         }
-        btnDownloadUserData.setOnClickListener {
+        binding.btnDownloadUserData.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 downloadUserData()
             }
         }
     }
 
-    private fun downloadUserData() {
+    private suspend fun downloadUserData() {
         for (i in 1..200000) {
-            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                binding.tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+           // Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
         }
     }
 }
