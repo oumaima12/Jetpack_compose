1deff0bbc82595737cae8ba1d703cccd1bad5afa
oumaima_makhlouk <oumaima.makhlouk@ibm.com>
2023-04-17 17:18:31 +0000

    coroutines-challenge : Main & background coroutines

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,39 +3,23 @@ package com.example.jetpackcompose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
-    private lateinit var btnDownloadUserData : Button
-    private lateinit var btnCount : Button
-    private lateinit var tvCount : TextView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnDownloadUserData = findViewById(R.id.btnDownloadUserData)
-        btnCount = findViewById(R.id.btnCount)
-        tvCount = findViewById(R.id.tvCount)
 
-        btnCount.setOnClickListener {
-            tvCount.text = count++.toString()
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.i("My tag", "Main Coroutines : ${Thread.currentThread().name}")
         }
-        btnDownloadUserData.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                downloadUserData()
-            }
-        }
-    }
 
-    private fun downloadUserData() {
-        for (i in 1..200000) {
-            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.i("My tag", "Background Coroutines :  ${Thread.currentThread().name}")
         }
+
     }
 }
